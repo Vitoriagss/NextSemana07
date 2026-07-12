fb2eb80ef4b03ad62df89a01a952a7ba79e8b20c
Vitoriagss <mariavitoriagsilva2018@gmail.com>
2026-07-12 19:36:20 -0300

    feat:Sessao hero

--- receitas/src/app/page.tsx
+++ receitas/src/app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Tela inicial</h1>
+
+      {/* seção Hero */}
+      <section className="bg-orange-50 py-12">
+        <div className="flex flex-col gap-6 items-center container mx-auto">
+          <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
+          <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
+
+          <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
+            Ver todas as receitas
+          </Link>
+        </div>
+      </section>
+
+
+
+
+
     </main>
   );
 }
